0bbcbee70a9861c919c6a1b213147ddbb72e72d1
Sagi Mines <sagi1236@gmail.com>
2022-08-02 16:39:32 +0300

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import PrimesGenerator from './publishers/primesGenerator.js';
-import marsenne from './clients/marsenne.js';
-import prime1 from './clients/prime1.js';
-import prime4Digits from './clients/prime4Digits.js';
+import { marsenne, marsenneCheck } from './clients/marsenne.js';
+import { prime1Check, prime1 } from './clients/prime1.js';
+import { prime4Digits, prime4DigitsCheck } from './clients/prime4Digits.js';
 
-export const start = limit => {
+export const startGenerator = limit => {
   const prime = new PrimesGenerator(limit);
   prime.on('prime1', prime1);
   prime.on('prime4', prime4Digits);
   prime.on('marsenne', marsenne);
+  const startGenerator = setInterval(() => {
+    let value;
+    do {
+      value = prime.getGenerator().next().value;
+    } while (
+      !prime1Check(value) &&
+      !marsenneCheck(value) &&
+      !prime4DigitsCheck(value)
+    );
 
-  prime.startGenerator();
+    if (!value) {
+      clearInterval(startGenerator);
+    } else {
+      prime.printPrime(value);
+    }
+  }, 1000);
 };
