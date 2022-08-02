9a637b7dbea554a43b08be71ba82038e7efea294
Sagi Mines <sagi1236@gmail.com>
2022-08-02 15:10:36 +0300

    Created the index.js file

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import marsenne from './clients/marsenne.js';
 import prime1 from './clients/prime1.js';
 import prime4Digits from './clients/prime4Digits.js';
 
-const prime = new PrimesGenerator(40);
-prime.on('prime1', prime1);
-prime.on('prime4', prime4Digits);
-prime.on('marsenne', marsenne);
+export const start = limit => {
+  const prime = new PrimesGenerator(limit);
+  prime.on('prime1', prime1);
+  prime.on('prime4', prime4Digits);
+  prime.on('marsenne', marsenne);
 
-const start = () => {
   prime.startGenerator();
 };
